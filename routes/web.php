940f638cc8f4caaaf9282a69e5c9d532b62d4614
patrahnakayenga <nakayengapatrah@gmail.com>
2025-01-14 18:28:27 +0300

    added auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\CourseController;
@@ -12,26 +14,45 @@ use App\Http\Controllers\ReportController;
 
 
 
+
+
+
 Route::get('/', function () {
     return view('layout');
 });
 
 
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+
 Route::resource('/students', Studentcontroller::class);
-
-
 Route::resource('/teachers', Teachercontroller::class);
-
 Route::resource('/courses', CourseController::class);
-
 Route::resource('/batches', BatchController::class);
-
-
 Route::resource('/enrollments', EnrollmentController::class);
-
 Route::resource('/payments', PaymentController::class);
-
 Route::get('report/report1/{pid}', [ReportController::class, 'report1']);
+});
+
+require __DIR__.'/auth.php';
+
+
+
+
 
 
 
